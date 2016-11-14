22696b6cd1ccd45c8ea7329d157d6d01ae0d5d3d
Minako Doi <minakod@hawaii.edu>
2016-11-14 01:16:15 -1000

    updated term of use

--- app/imports/ui/pages/term-page.js
+++ app/imports/ui/pages/term-page.js
@@ -3,7 +3,7 @@ import { Template } from 'meteor/templating';
 
 /* eslint-disable object-shorthand */
 
-Session.setDefault('key', true);
+Session.setDefault('key', false);
 
 Template.Term_Page.helpers({
   /**
@@ -11,17 +11,15 @@ Template.Term_Page.helpers({
    */
   user: function user() {
     return Meteor.user() ? Meteor.user().profile.name : 'No logged in user';
-  },
+  }
+});
 
+Template.Term_Page.events({
   'change #checker': function() {
     if(document.getElementById('checker').checked)
       Session.set('key', true);
     else
       Session.set('key', false);
-  },
-
-  isChecked: function() {
-    return this.condition ? { checked: true } : { checked: false };
   }
 });
 
