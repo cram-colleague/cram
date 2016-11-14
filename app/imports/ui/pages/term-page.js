38db2698a52076f33e746369ec48b286d06ef543
Minako Doi <minakod@hawaii.edu>
2016-11-13 23:53:01 -1000

    still working on term of use

--- app/imports/ui/pages/term-page.js
+++ app/imports/ui/pages/term-page.js
@@ -3,6 +3,8 @@ import { Template } from 'meteor/templating';
 
 /* eslint-disable object-shorthand */
 
+Session.setDefault('key', true);
+
 Template.Term_Page.helpers({
   /**
    * @returns {String} Returns the user who's logged in
@@ -11,7 +13,20 @@ Template.Term_Page.helpers({
     return Meteor.user() ? Meteor.user().profile.name : 'No logged in user';
   },
 
-  'click input': function(event) {
-    window.alert('Hi');
+  'change #checker': function() {
+    if(document.getElementById('checker').checked)
+      Session.set('key', true);
+    else
+      Session.set('key', false);
+  },
+
+  isChecked: function() {
+    return this.condition ? { checked: true } : { checked: false };
+  }
+});
+
+Template.check.helpers({
+  'isTrue': function() {
+    return Session.get('key');
   }
 });
