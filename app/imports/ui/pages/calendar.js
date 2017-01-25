b64d8940e6d47c437b72248d8bbc9ea1bf9e5970
minakodoi <minako@MinakonoMacBook-Pro.local>
2017-01-24 14:20:28 -1000

    Updated

--- app/imports/ui/pages/calendar.js
+++ app/imports/ui/pages/calendar.js
@@ -24,38 +24,20 @@ Template.Calendar_Page.onRendered( () => {
       if ( data ) {
         callback( data );
       }
-    }
+    },
+    eventRender( event, element ) {
+      element.find( '.fc-content' ).html(
+          `<h4>${ event.name }</h4>
+         <p class="sensei">${ event.sensei }</p>
+         <p class="students">${ event.students }</p>
+        `
+      );
+    },
+    dayClick( date ) {
+      Session.set( 'eventModal', { type: 'add', date: date.format()     } );
+      $( '#addSession' ).modal( 'show' );
+    },
   });
-  // $( '#events-calendar' ).fullCalendar({
-    // events( start, end, timezone, callback ) {
-    //   let data = SSession.find().fetch().map( ( event ) => {
-    //     event.editable = !isPast( event.start );
-    //     return event;
-    //   });
-    //
-    //   if ( data ) {
-    //     callback( data );
-    //   }
-    // },
-    // eventRender( event, element ) {
-    //   element.find( '.fc-content' ).html(
-    //       `<h4>${ event.title }</h4>
-    //      <p class="guest-count">${ event.guests } Guests</p>
-    //      <p class="type-${ event.type }">#${ event.type }</p>
-    //     `
-    //   );
-    // },
-    // dayClick( date ) {
-    //   FlowRouter.go('Add_Session_Page');
-      //Session.set( 'eventModal', { type: 'add', date: date.format() } );
-      //$( '#add-edit-event-modal' ).modal( 'show' );
-    // },
-    // eventClick( event ) {
-    //   FlowRouter.go('Add_Session_Page');
-    //   //Session.set( 'eventModal', { type: 'edit', event: event._id } );
-    //   //$( '#add-edit-event-modal' ).modal( 'show' );
-    // }
-  // });
   Tracker.autorun( () => {
     SSession.find().fetch();
     $( '#events-calendar' ).fullCalendar( 'refetchEvents' );
